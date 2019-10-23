b62cc2f5abf8872578c0808e81f09aec54be1378
Abdur-Raheem Lee <lee13october@gmail.com>
2019-10-23 12:59:23 +0200

    iTunes App

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
